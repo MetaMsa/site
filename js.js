48dfe42219151fc0a02bf57e3e1bf962d0f450c1
MetaMsa <mserhataslan@hotmail.com>
2024-01-02 23:56:00 +0300

    main

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
-var merhaba="MERHAB";
-for(let j=0;j<6;j++)
-{  
-setTimeout(() => { document.getElementById("merhaba").innerHTML+=merhaba[j]; },8000) ;
+var merhaba="MERHABA",j=0;
+while(1)
+{
+    setTimeout(() => { document.getElementById("merhaba").innerHTML+=merhaba[j]; j++;if(j=6){j=0}},8000) ;
 }
