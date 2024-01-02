3d1f354f314a5770f36f8785405a8c034f62cb1f
MetaMsa <mserhataslan@hotmail.com>
2024-01-03 02:10:53 +0300

    main

--- js.js
+++ js.js
@@ -1,6 +1,1 @@
-var merhaba="MERHABA";
-for(let i=0;i<7;i++)
-{
-    for(let j=0;j<1000000;j++);
-    document.getElementById("merhaba").innerHTML+=merhaba[i];
-}
+document.getElementById("merhaba").innerHTML="MERHABA";
