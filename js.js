8fa701c2bd7ebbfda32ee55d43461bf0c391692d
MetaMsa <90111767+MetaMsa@users.noreply.github.com>
2024-01-02 22:52:33 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,8 +1,8 @@
 var merhaba="MERHABA";
-for(let j=0;<10;j++)
+for(let j=0;<7;j++)
 {  
 for(let i=0;i<10;i++)
 {
-document.getElementById("merhaba").innerHTML+=merhaba[i];
+document.getElementById("merhaba").innerHTML+=merhaba[j];
 }
 }
